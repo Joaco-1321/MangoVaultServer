a64bad8bd45c2352433355353219c58ed5366583
Joaco-1321 <126332412+Joaco-1321@users.noreply.github.com.>
2024-05-02 11:21:17 +0200

    feat: add Message model

---
+++ src/main/java/io/joaco/mangovaultserver/Message.java
@@ -0,0 +1,40 @@
+package io.joaco.mangovaultserver;
+
+public class Message {
+    private String from;
+    private String to;
+    private String message;
+
+    public Message() {
+    }
+
+    public Message(String from, String to, String message) {
+        this.from = from;
+        this.to = to;
+        this.message = message;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
