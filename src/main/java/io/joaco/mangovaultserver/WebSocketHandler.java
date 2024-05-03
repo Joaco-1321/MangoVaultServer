72c4aa324b5c07fdad4a1cd1e8686f9b564588be
Joaco-1321 <126332412+Joaco-1321@users.noreply.github.com.>
2024-05-03 18:59:00 +0200

    feat: add authentication after established connection
    now the handleTextMessage method handles authentication
    differetiating between an authentication json and a
    message json

--- src/main/java/io/joaco/mangovaultserver/WebSocketHandler.java
+++ src/main/java/io/joaco/mangovaultserver/WebSocketHandler.java
@@ -1,12 +1,19 @@
 package io.joaco.mangovaultserver;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.joaco.mangovaultserver.model.Message;
+import io.joaco.mangovaultserver.model.User;
+import io.joaco.mangovaultserver.services.AuthService;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
@@ -14,26 +21,40 @@ public class WebSocketHandler extends TextWebSocketHandler {
     private static final Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
 
     @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        String username = extractUsernameFromQueryString(session.getUri().getRawQuery());
-        sessions.put(username, session);
-    }
-
-    @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        // maps json to objects
         ObjectMapper mapper = new ObjectMapper();
-        Message msg = mapper.readValue(message.getPayload(), Message.class);
+        // creates a node tree with the values of the json
+        JsonNode jsonNode = mapper.readTree(message.getPayload());
 
-        // Get recipient's WebSocket session
-        WebSocketSession recipientSession = sessions.get(msg.getTo());
-        if (recipientSession != null) {
-            // Send the message to the recipient's WebSocket session
-            recipientSession.sendMessage(new TextMessage(msg.getMessage()));
+        // if the json contains username and password, authenticates the user
+        // else, closes the connection
+        if (jsonNode.has("username") && jsonNode.has("password")) {
+            Optional<User> authenticatedUser = AuthService.authenticate(
+                    jsonNode.get("username").asText(),
+                    jsonNode.get("password").asText()
+            );
+
+            ObjectNode response = mapper.createObjectNode();
+
+            if (authenticatedUser.isPresent()) {
+                sessions.put(authenticatedUser.get().getUsername(), session);
+                response.put("success", true);
+                session.sendMessage(new TextMessage(response.toString()));
+            } else {
+                response.put("success", false);
+                session.sendMessage(new TextMessage(response.toString()));
+                session.close(CloseStatus.PROTOCOL_ERROR.withReason("authentication failed"));
+            }
+        } else {
+            // map json to Message object
+            Message msg = mapper.readValue(message.getPayload(), Message.class);
+            // get the session of recipient
+            WebSocketSession recipientSession = sessions.get(msg.getTo());
+
+            if (recipientSession != null) {
+                recipientSession.sendMessage(new TextMessage(message.getPayload()));
+            }
         }
-    }
-
-    private String extractUsernameFromQueryString(String queryString) {
-        String[] parts = queryString.split("=");
-        return parts[1];
     }
 }
