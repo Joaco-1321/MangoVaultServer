a48be78efc7caa36842d22da4dcb4fa48b5d6937
Joaco-1321 <126332412+Joaco-1321@users.noreply.github.com.>
2024-05-12 22:44:07 +0200

    feat: implement ChatController

--- src/main/java/io/joaco/mangovaultserver/controller/ChatController.java
+++ src/main/java/io/joaco/mangovaultserver/controller/ChatController.java
@@ -1,4 +1,20 @@
 package io.joaco.mangovaultserver.controller;
 
+import io.joaco.mangovaultserver.model.Message;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@RequiredArgsConstructor
+@Controller
 public class ChatController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/chat")
+    public void handle(Message message) {
+        System.out.println(message.toString());
+        messagingTemplate.convertAndSendToUser(message.getTo(), "/queue/chat", message);
+    }
 }
