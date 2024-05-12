e8b136af901f3b1d7dcfff74e7b67debb2081929
Joaco-1321 <126332412+Joaco-1321@users.noreply.github.com.>
2024-05-12 22:40:51 +0200

    refactor: modify Message model to use Lombok annotations

--- src/main/java/io/joaco/mangovaultserver/model/Message.java
+++ src/main/java/io/joaco/mangovaultserver/model/Message.java
@@ -1,40 +1,18 @@
 package io.joaco.mangovaultserver.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Message {
+
     private String from;
     private String to;
     private String message;
-
-    public Message() {
-    }
-
-    public Message(String from, String to, String message) {
-        this.from = from;
-        this.to = to;
-        this.message = message;
-    }
-
-    public String getFrom() {
-        return from;
-    }
-
-    public void setFrom(String from) {
-        this.from = from;
-    }
-
-    public String getTo() {
-        return to;
-    }
-
-    public void setTo(String to) {
-        this.to = to;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
+
