2db9c3577156e226c17d2b7c14d7e464240e3dc5
Joaco-1321 <126332412+Joaco-1321@users.noreply.github.com.>
2024-05-19 18:30:33 +0200

    feat: add messages to UserData validation

--- src/main/java/io/joaco/mangovaultserver/dto/UserData.java
+++ src/main/java/io/joaco/mangovaultserver/dto/UserData.java
@@ -6,9 +6,20 @@ import lombok.Data;
 @Data
 public class UserData {
 
-    @Pattern(regexp = "^[a-zA-Z]\\w{2,}[a-zA-Z0-9]$")
+    @Pattern(regexp = "^[a-zA-Z]\\w{2,}[a-zA-Z0-9]$",
+             message = """
+                       must be at least 4 characters long.
+                       must begin with a letter.
+                       can contain letters, numbers and "_".
+                       must not end with an "_".""")
     private String username;
 
-    @Pattern(regexp = "^(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\\w\\s:])(\\S){8,16}$")
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\\w\\s:])(\\S){8,16}$",
+             message = """
+                       must be between 8 and 16 characters long with no space.
+                       must contain at least 1 number.
+                       must contain at least 1 uppercase letter.
+                       must contain at least 1 lowercase letter.
+                       must contain at least 1 non-alphanumeric character.""")
     private String password;
 }
