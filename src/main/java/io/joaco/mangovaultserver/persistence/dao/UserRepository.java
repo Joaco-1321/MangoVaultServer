768460ea729a2b50457e28ce5e5c5a2139ea37c2
Joaco-1321 <126332412+Joaco-1321@users.noreply.github.com.>
2024-05-19 16:26:03 +0200

    fix: add Repository annotation to UserRepository

--- src/main/java/io/joaco/mangovaultserver/persistence/dao/UserRepository.java
+++ src/main/java/io/joaco/mangovaultserver/persistence/dao/UserRepository.java
@@ -2,9 +2,11 @@ package io.joaco.mangovaultserver.persistence.dao;
 
 import io.joaco.mangovaultserver.persistence.model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
