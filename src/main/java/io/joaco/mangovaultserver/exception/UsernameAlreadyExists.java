db917772bd2fcfc276027d83952eaf7a8b8ac137
Joaco-1321 <126332412+Joaco-1321@users.noreply.github.com.>
2024-05-19 16:55:11 +0200

    feat: add UsernameAlreadyExists exception

---
+++ src/main/java/io/joaco/mangovaultserver/exception/UsernameAlreadyExists.java
@@ -0,0 +1,5 @@
+package io.joaco.mangovaultserver.exception;
+
+public class UsernameAlreadyExists extends Throwable {
+
+}
---
+++ src/main/java/io/joaco/mangovaultserver/service/UserService.java
@@ -0,0 +1,38 @@
+package io.joaco.mangovaultserver.service;
+
+import io.joaco.mangovaultserver.dto.UserData;
+import io.joaco.mangovaultserver.persistence.dao.UserRepository;
+import io.joaco.mangovaultserver.persistence.model.Role;
+import io.joaco.mangovaultserver.persistence.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public void register(UserData user) {
+        if (userExists(user.getUsername())) {
+            return;
+        }
+
+        userRepository.save(User.builder()
+                                .username(user.getUsername())
+                                .password(passwordEncoder.encode(user.getPassword()))
+                                .enabled(true)
+                                .role(Role.builder()
+                                          .role("USER")
+                                          .build())
+                                .build());
+    }
+
+    private boolean userExists(String username) {
+        return userRepository.findByUsername(username)
+                             .isPresent();
+    }
+}
