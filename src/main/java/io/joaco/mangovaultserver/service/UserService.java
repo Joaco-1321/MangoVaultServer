a3f3ab6765812d5e7e87dc3b0f768de05a3e2a25
Joaco-1321 <126332412+Joaco-1321@users.noreply.github.com.>
2024-05-19 18:29:29 +0200

    fix: add RoleRepository to UserService

--- src/main/java/io/joaco/mangovaultserver/service/UserService.java
+++ src/main/java/io/joaco/mangovaultserver/service/UserService.java
@@ -2,6 +2,7 @@ package io.joaco.mangovaultserver.service;
 
 import io.joaco.mangovaultserver.dto.UserData;
 import io.joaco.mangovaultserver.exception.UsernameAlreadyExists;
+import io.joaco.mangovaultserver.persistence.dao.RoleRepository;
 import io.joaco.mangovaultserver.persistence.dao.UserRepository;
 import io.joaco.mangovaultserver.persistence.model.Role;
 import io.joaco.mangovaultserver.persistence.model.User;
@@ -15,21 +16,24 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    private final RoleRepository roleRepository;
+
     private final PasswordEncoder passwordEncoder;
 
     public void register(UserData user) throws UsernameAlreadyExists {
         if (userExists(user.getUsername())) {
-            throw new UsernameAlreadyExists();
+            throw new UsernameAlreadyExists("username already exists");
         }
 
-        userRepository.save(User.builder()
-                                .username(user.getUsername())
-                                .password(passwordEncoder.encode(user.getPassword()))
-                                .enabled(true)
-                                .role(Role.builder()
-                                          .role("USER")
-                                          .build())
+        roleRepository.save(Role.builder()
+                                .role("USER")
+                                .user(userRepository.save(User.builder()
+                                                              .username(user.getUsername())
+                                                              .password(passwordEncoder.encode(user.getPassword()))
+                                                              .enabled(true)
+                                                              .build()))
                                 .build());
+        ;
     }
 
     private boolean userExists(String username) {
