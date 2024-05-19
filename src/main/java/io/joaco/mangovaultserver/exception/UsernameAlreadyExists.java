75a0fdc160db70e53568a7ca3547a3a06902ac34
Joaco-1321 <126332412+Joaco-1321@users.noreply.github.com.>
2024-05-19 18:28:06 +0200

    fix: change base class from Throwable to RuntimeException

--- src/main/java/io/joaco/mangovaultserver/exception/UsernameAlreadyExists.java
+++ src/main/java/io/joaco/mangovaultserver/exception/UsernameAlreadyExists.java
@@ -1,5 +1,19 @@
 package io.joaco.mangovaultserver.exception;
 
-public class UsernameAlreadyExists extends Throwable {
+public class UsernameAlreadyExists extends RuntimeException {
 
+    public UsernameAlreadyExists() {
+    }
+
+    public UsernameAlreadyExists(String message) {
+        super(message);
+    }
+
+    public UsernameAlreadyExists(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UsernameAlreadyExists(Throwable cause) {
+        super(cause);
+    }
 }
